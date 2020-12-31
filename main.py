c96eb361f3421145aad7c36a262bc41689cfb5bb
jaideep1163 <66325920+jaideep1163@users.noreply.github.com>
2020-12-31 11:14:58 +0530

    Restored Previous Code

--- main.py
+++ main.py
@@ -94,3 +94,135 @@ def vsCode():
 
     # waits for 3 seconds
     time.sleep(3)
+
+def exit():
+    #This Function Is Used to Quit the Windows Using the Quit Button 
+    sys.exit()
+# Opening Functions
+
+def chrome():
+    """ 
+    This Function opens spotlight,
+    waits for 1 second, then opens chrome.
+    after chrome opens, it 
+    """
+    if sys.platform.startswith('win32'):
+        chrome_windows()
+        return
+    # holds command
+    pg.keyDown('command')
+    # presses space
+    pg.hotkey('space')
+    # releases command
+    pg.keyUp('command')
+    
+    # waits for 1 second
+    time.sleep(1)
+
+    # types chrome in 0.3 seconds
+    pg.typewrite('chrome\n', 0.3)
+
+    # waits for 0.5 seconds
+    time.sleep(0.5)
+
+# ---------------------------
+
+def terminal():
+    """ 
+    This Function opens spotlight,
+    waits for 1 second, then opens terminal.
+    """
+    # holds command
+    pg.keyDown('command')
+    # presses space
+    pg.hotkey('space')
+    # releases command
+    pg.keyUp('command')
+    
+    # waits for 1 second
+    time.sleep(1)
+
+    # types terminal with pauses of 0.3 seconds
+    pg.typewrite('terminal\n', 0.3)
+
+    # waits for 0.5 seconds
+    time.sleep(0.5)
+
+# ---------------------------
+
+def discord():
+    """ 
+    This Function opens spotlight,
+    waits for 1 second, then opens discord
+    """
+    # holds command
+    pg.keyDown('command')
+    # presses space
+    pg.hotkey('space')
+    # releases command
+    pg.keyUp('command')
+    
+    # waits for 1 second
+    time.sleep(1)
+
+    # types discord with pauses of 0.3 seconds
+    pg.typewrite('discord\n', 0.3)
+
+    # waits for 0.5 seconds
+    time.sleep(0.5)
+
+# ---------------------------
+
+def vsCode():
+    """ 
+    This Function opens spotlight,
+    waits for 1 second, then opens VS Code.
+    """
+    # holds command
+    pg.keyDown('command')
+    # presses space
+    pg.hotkey('space')
+    # releases command
+    pg.keyUp('command')
+    
+    # waits for 1 second
+    time.sleep(1)
+
+    # types Visual Studio Code with pauses of 0.2 seconds
+    pg.typewrite('visual studio code\n', 0.2)
+
+    # waits for 0.5 seconds
+    time.sleep(0.5)
+
+# Setting Buttons
+
+Button(text="Open Chrome", height=3, width=30, command=chrome).place(relx = 0.023, rely=0.010)
+
+Button(text="Open Discord", height=3, width=30, command = discord).place(relx = 0.023, rely=0.2)
+
+Button(text="Open terminal", height=3, width=30, command = terminal).place(relx = 0.023, rely=0.4)
+
+Button(text="Visual Studio Code", height=3, width=30, command = vsCode).place(relx = 0.023, rely=0.6)
+
+Button(text="Test", height=3, width=30).place(relx = 0.023, rely=0.8)
+
+Button(text="Test", height=3, width=30).place(relx = 0.023, rely=1)
+
+Button(text="Test", height=3, width=30).place(relx = 0.7, rely=0.010)
+
+Button(text="Test", height=3, width=30).place(relx = 0.7, rely=0.2)
+
+Button(text="Test", height=3, width=30).place(relx = 0.7, rely=0.4)
+
+Button(text="Test", height=3, width=30).place(relx = 0.7, rely=0.6)
+
+Button(text="Test", height=3, width=30).place(relx = 0.7, rely=0.8)
+
+# Quit Button
+
+quit_button = Button(text="Exit", bg="red", height=3, width=30, command=exit)
+quit_button.place(relx = 0.355, rely=0.4)
+
+# MainLoop
+
+window.mainloop()
