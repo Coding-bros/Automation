0c964c60b521060999ce832b866ee6b31f0fb3e5
Karthik <bkp.karthi@gmail.com>
2020-12-31 11:22:16 +0530

    updated code for windows and linux and mac

--- main.py
+++ main.py
@@ -2,95 +2,170 @@
 import pyautogui as pg
 import time 
 
-def chrome():
-    """ 
-    This Function opens spotlight,
-    waits for 1 second, then opens chrome.
-    after chrome opens, it makes the chrome full screen
-    """
-    # holds command
-    pg.keyDown('command')
-    # presses space
-    pg.hotkey('space')
-    # releases command
-    pg.keyUp('command')
-    
-    # waits for 1 second
-    time.sleep(1)
+#-----------------------------
 
-    # types chrome in 0.3 seconds
-    pg.typewrite('chrome\n', 0.3)
+ask = input('Are You On Mac Or Windows Or Linux: ')
 
-    # waits for 3 seconds
-    time.sleep(3)
+if ask.strip().lower() == 'mac':
+    def chromeM():
+        """ 
+        This Function opens spotlight,
+        waits for 1 second, then opens chrome.
+        """
+        # holds command
+        pg.keyDown('command')
+        # presses space
+        pg.hotkey('space')
+        # releases command
+        pg.keyUp('command')
+        
+        # waits for 1 second
+        time.sleep(1)
 
-# ---------------------------
+        # types chrome in 0.3 seconds
+        pg.typewrite('chrome\n', 0.3)
 
-def terminal():
-    """ 
-    This Function opens spotlight,
-    waits for 1 second, then opens terminal.
-    after terminal opens, it makes the terminal full screen
-    """
-    # holds command
-    pg.keyDown('command')
-    # presses space
-    pg.hotkey('space')
-    # releases command
-    pg.keyUp('command')
-    
-    # waits for 1 second
-    time.sleep(1)
+        # waits for 3 seconds
+        time.sleep(3)
 
-    # types terminal with pauses of 0.3 seconds
-    pg.typewrite('terminal\n', 0.3)
-
-    # waits for 3 seconds
-    time.sleep(3)
-
-# ---------------------------
-
-def discord():
-    """ 
-    This Function opens spotlight,
-    waits for 1 second, then opens discord.
-    after discord opens, it makes the discord full screen
-    """
-    # holds command
-    pg.keyDown('command')
-    # presses space
-    pg.hotkey('space')
-    # releases command
-    pg.keyUp('command')
-    
-    # waits for 1 second
-    time.sleep(1)
-
-    # types discord with pauses of 0.3 seconds
-    pg.typewrite('discord\n', 0.3)
-
-    # waits for 3 seconds
-    time.sleep(3)
     # ---------------------------
 
-def vsCode():
-    """ 
-    This Function opens spotlight,
-    waits for 1 second, then opens vsCode.
-    after vsCode opens, it makes the vsCode full screen
-    """
-    # holds command
-    pg.keyDown('command')
-    # presses space
-    pg.hotkey('space')
-    # releases command
-    pg.keyUp('command')
-    
-    # waits for 1 second
-    time.sleep(1)
+    def terminalM():
+        """ 
+        This Function opens spotlight,
+        waits for 1 second, then opens terminal.
+        """
+        # holds command
+        pg.keyDown('command')
+        # presses space
+        pg.hotkey('space')
+        # releases command
+        pg.keyUp('command')
+        
+        # waits for 1 second
+        time.sleep(1)
 
-    # types vsCode with pauses of 0.3 seconds
-    pg.typewrite('visual studio code\n', 0.3)
+        # types terminal with pauses of 0.3 seconds
+        pg.typewrite('terminal\n', 0.3)
 
-    # waits for 3 seconds
-    time.sleep(3)
+        # waits for 3 seconds
+        time.sleep(3)
+
+    # ---------------------------
+
+    def discordM():
+        """ 
+        This Function opens spotlight,
+        waits for 1 second, then opens discord.
+        """
+        # holds command
+        pg.keyDown('command')
+        # presses space
+        pg.hotkey('space')
+        # releases command
+        pg.keyUp('command')
+        
+        # waits for 1 second
+        time.sleep(1)
+
+        # types discord with pauses of 0.3 seconds
+        pg.typewrite('discord\n', 0.3)
+
+        # waits for 3 seconds
+        time.sleep(3)
+        # ---------------------------
+
+    def vsCodeM():
+        """ 
+        This Function opens spotlight,
+        waits for 1 second, then opens vsCode.
+        """
+        # holds command
+        pg.keyDown('command')
+        # presses space
+        pg.hotkey('space')
+        # releases command
+        pg.keyUp('command')
+        
+        # waits for 1 second
+        time.sleep(1)
+
+        # types vsCode with pauses of 0.3 seconds
+        pg.typewrite('visual studio code\n', 0.3)
+
+        # waits for 3 seconds
+        time.sleep(3) 
+# ---------------------------------
+if ask.strip().lower() == 'windows' or ask.strip().lower() == 'linux':
+    def chrome():
+        """ 
+        This Function opens start menu,
+        waits for 1 second, then opens chrome.
+        """
+        # presses the start menu button
+        pg.hotkey('winleft')
+        
+        # waits for 1 second
+        time.sleep(1)
+
+        # types chrome in 0.3 seconds
+        pg.typewrite('chrome\n', 0.3)
+
+        # waits for 3 seconds
+        time.sleep(3)
+
+    # ---------------------------
+
+    def terminal():
+        """ 
+        This Function opens start menu,
+        waits for 1 second, then opens terminal.
+        """
+        # presses the start menu button
+        pg.hotkey('winleft')
+        
+        # waits for 1 second
+        time.sleep(1)
+
+        # types terminal with pauses of 0.3 seconds
+        pg.typewrite('terminal\n', 0.3)
+
+        # waits for 3 seconds
+        time.sleep(3)
+
+    # ---------------------------
+
+    def discord():
+        """ 
+        This Function opens start menu,
+        waits for 1 second, then opens discord.
+        """
+        # presses the start menu button
+        pg.hotkey('winleft')
+        
+        # waits for 1 second
+        time.sleep(1)
+
+        # types discord with pauses of 0.3 seconds
+        pg.typewrite('discord\n', 0.3)
+
+        # waits for 3 seconds
+        time.sleep(3)
+        # ---------------------------
+
+    def vsCode():
+        """ 
+        This Function opens start menu,
+        waits for 1 second, then opens vsCode.
+        """
+        # presses the start menu button
+        pg.hotkey('winleft')
+        
+        # waits for 1 second
+        time.sleep(1)
+
+        # types vsCode with pauses of 0.3 seconds
+        pg.typewrite('visual studio code\n', 0.3)
+
+        # waits for 3 seconds
+        time.sleep(3) 
